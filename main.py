6ce9617cd6a0e64b5dc0fda989ace5cb4789ff48
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2021-03-06 18:18:52 +0900

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import os
 from discord.ext import commands
 import discord
+import os
 
+bot = commands.Bot(command_prefix="s/")
 
-if __name__ == "__main__":
-  bot = commands.Bot(command_prefix="s/")
-  TOKEN = os.getenv("sumirebot_token")
-  bot.run(TOKEN)
+TOKEN = os.getenv("sumire_bot_token")
+bot.run(TOKEN)
