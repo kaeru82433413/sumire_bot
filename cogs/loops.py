d044321c1dd2bba61ffa41e30513fd5080cb31fa
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2021-05-03 00:08:25 +0900

    同日の複数実績に対応

--- cogs/loops.py
+++ cogs/loops.py
@@ -20,10 +20,17 @@ class Loops(commands.Cog):
     async def daily(self, now):
         date = now.month, now.day
         if date in seichi_data.ACHIEVEMENTS:
-            ach_id, ach_name = seichi_data.ACHIEVEMENTS[date]
+            achievement = seichi_data.ACHIEVEMENTS[date]
             regular_notice_ch = self.bot.get_channel(820939592999108648)
             embed = discord.Embed(title="整地鯖の記念日実績が解除できます", color=0xffff00)
-            embed.description = f"本日は 「{ach_name}」です\n実績No{ach_id}を解除していない人は忘れずに解除しましょう"
+
+            if not isinstance(achievement, list):
+                ach_id, ach_name = achievement
+                embed.description = f"本日は 「{ach_name}」です\n実績No{ach_id}を解除していない人は忘れずに解除しましょう"
+            else:
+                ach_ids, ach_names = zip(*achievement)
+                embed.description = f"本日は 「{'」と「'.join(ach_names)}」です\n実績No{','.join(map(str, ach_ids))}を解除していない人は忘れずに解除しましょう"
+
             await regular_notice_ch.send(embed=embed)
 
 
