abf5ae69e9e9e28e8b97a9304757b7a7aea4cfb3
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2021-10-31 15:44:16 +0900

    ポイントを記録

--- cogs/loops.py
+++ cogs/loops.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands, tasks
-from datetime import datetime
+from datetime import datetime, date
+from operator import attrgetter
 import seichi_data
+
 
 class Loops(commands.Cog):
     def __init__(self, bot):
@@ -30,6 +32,9 @@ class Loops(commands.Cog):
             embed.description = f"本日は 「{'」と「'.join(ach_names)}」です\n実績No{','.join(map(str, ach_ids))}を解除していない人は忘れずに解除しましょう"
 
             await regular_notice_ch.send(seichi_acv_ntc_role.mention, embed=embed)
+        
+        self.record_point(now.date())
+
     
     async def daily22h(self, now):
         today_achievements = seichi_data.get_today_achievements(now)
@@ -43,6 +48,15 @@ class Loops(commands.Cog):
 
             await regular_notice_ch.send(seichi_acv_ntc_role.mention, embed=embed)
     
+
+    def record_point(self, today: date):
+        members = {member.id: member for member in self.bot.sumire_server.members if not member.bot}
+        members_data = self.bot.members_data(map(attrgetter("id"), members.values()), ("id", "point"))
+        for id_, point in members_data:
+            member = members[id_]
+            self.bot.postgres("insert into point_record (id, point, display_name, date) values (%s, %s, %s, %s);",
+                               id_, point, member.display_name, today)
+
     
     @minutely.error
     async def minutely_error(self, error):
